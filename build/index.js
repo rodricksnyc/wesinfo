5e60f1c6017cb5a9ff0931fc277bc197d3e3178e
Elizabeth <elizabeth@Elizabeths-iMac.local>
2020-08-18 18:28:23 -0400

    redo babel

--- build/index.js
+++ build/index.js
@@ -1,13 +1,16 @@
-"use strict";
+'use strict';
 
 $(document).ready(function () {
+
   $('.carousel').on('slid.bs.carousel', function () {
     var carouselData = $(this).data('bs.carousel');
+
     var currentIndex = $(this).find(".active").index();
+
     $('.result p').removeClass('active-p').eq(currentIndex).addClass('active-p');
   });
-  var block = {
-    "blocks": [{
+
+  var block = { "blocks": [{
       "title": 'Forms',
       "icon": '<i class="fal fa-file-alt"></i>'
     }, {
@@ -26,7 +29,7 @@ $(document).ready(function () {
       "title": 'Proposal Central',
       "icon": '<i class="fal fa-folders"></i>'
     }, {
-      "title": 'Institutional  Review Board',
+      "title": 'Proposal Central',
       "icon": '<i class="fal fa-search"></i>'
     }, {
       "title": 'Training',
@@ -38,9 +41,11 @@ $(document).ready(function () {
       "title": 'Workday',
       "icon": '<i class="fal fa-user-chart"></i>'
     }]
+
   };
 
   for (var j in block.blocks) {
-    $('.popularR').append("<div class=\"col-lg-2 col-md-6 col-sm-12 col-xs-12\"><div class=\"smallBubble d-flex flex-column align-items-center\">" + block.blocks[j].icon + "<p class=\"blue\">" + block.blocks[j].title + "</p></div></div>");
+
+    $('.popularR').append('<div class="col-lg-2 col-md-6 col-sm-12 col-xs-12"><div class="smallBubble d-flex flex-column align-items-center">' + block.blocks[j].icon + '<p class="blue">' + block.blocks[j].title + '</p></div></div>');
   }
 });
