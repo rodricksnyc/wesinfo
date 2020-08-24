fb791108518424825529f2ce39d69ccbc094153c
Elizabeth <elizabeth@Elizabeths-iMac.local>
2020-08-24 09:42:37 -0400

    WEfffefewfewfewwf

--- src/config.js
+++ src/config.js
@@ -1,21 +1,55 @@
 
 
-const block = {
-    "blocks": [
-      {
-        "title": 'Forms',
-        "icon": '<i class="fal fa-file-alt"></i>'
-      },
+const block = {"blocks": [{
+  "title": 'Forms',
+  "icon": '<i class="fal fa-file-alt"></i>'
+},
 
-      {
-        "title": 'Schedules',
-        "icon": '<i class="fal fa-calendar-alt"></i>'
-      },
+{
+  "title": 'Schedules',
+  "icon": '<i class="fal fa-calendar-alt"></i>'
+},
 
-      {
-        "title": 'Workday',
-        "icon": '<i class="fal fa-user-chart"></i>'
-      }]
+{
+  "title": 'Policies',
+  "icon": '<i class="fal fa-clipboard-list-check"></i>'
+},
+
+{
+  "title": 'On Campus',
+  "icon": '<i class="fal fa-building"></i>'
+},
+
+{
+  "title": 'OAsys',
+  "icon": '<i class="fal fa-desktop-alt"></i>'
+},
+
+{
+  "title": 'Proposal Central',
+  "icon": '<i class="fal fa-folders"></i>'
+},
+{
+  "title": 'Proposal Central',
+  "icon": '<i class="fal fa-search"></i>'
+},
+{
+  "title": 'Training',
+  "icon": '<i class="fal fa-users-class"></i>'
+},
+{
+  "title": 'Benefits',
+  "icon": '<i class="fal fa-rocket-launch"></i>'
+},
+
+{
+  "title": 'Workday',
+  "icon": '<i class="fal fa-user-chart"></i>'
+}
+
+
+]
+
 };
 
 }
